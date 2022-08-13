e3fc917b8d9efe2cb207a53804db535523f51b25
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-08-13 23:03:51 +0800

    refactor: use localStorage to get better search experience

--- src/components/ListOfCountries.jsx
+++ src/components/ListOfCountries.jsx
@@ -1,47 +1,35 @@
 import { useEffect, useState } from "react";
-import { useDebounce } from "../hooks/useDebounce";
 import { Link } from "react-router-dom";
-import { Flex, Center, Button, Input } from "@chakra-ui/react";
-import axios from "axios";
+import { Flex, Input } from "@chakra-ui/react";
 import PreviewCard from "./PreviewCard";
 import FilterBox from "./FilterBox";
 
 const ListOfCountries = ({ cca3 }) => {
   const [data, setData] = useState(null);
-  const [dataLength, setDataLength] = useState(null);
-  const [limit, setLimit] = useState(12);
-  const [apiUrl, setApiUrl] = useState("https://restcountries.com/v3.1/all");
   const [searchQuery, setSearchQuery] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [optionUrl, setOptionUrl] = useState("");
 
+  const apiUrl = "https://restcountries.com/v3.1/all";
+
   useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.get(apiUrl);
-        setErrorMessage("");
-        setDataLength(response.data.length);
-        setData(response.data.slice(0, limit));
-        if (searchQuery === "") {
-          optionUrl
-            ? setApiUrl(optionUrl)
-            : setApiUrl("https://restcountries.com/v3.1/all");
-        } else {
-          setApiUrl(
-            `https://restcountries.com/v3.1/name/${encodeURI(
-              searchQuery
-            )}?fullText=true`
-          );
-        }
-      } catch (error) {
-        setDataLength(0);
-        setErrorMessage(
-          `${searchQuery} is not found, plesase enter full name of country`
-        );
-        if (searchQuery === "") setApiUrl("https://restcountries.com/v3.1/all");
-      }
-    })();
-  }, [limit, apiUrl, searchQuery, optionUrl]);
+    if (localStorage.getItem("countriesData")) {
+      const result = JSON.parse(localStorage.getItem("countriesData"));
+      searchQuery
+        ? setData(
+            result.filter((x) =>
+              x.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+            )
+          )
+        : setData(result);
+    } else
+      (async () => {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setData(data);
+        localStorage.setItem("countriesData", JSON.stringify(data));
+      })();
+  }, [searchQuery]);
 
   return (
     <>
@@ -55,9 +43,9 @@ const ListOfCountries = ({ cca3 }) => {
           placeholder="Search for a country..."
           maxW="480px"
           boxShadow="sm"
-          onChange={useDebounce((e) => setSearchQuery(e.target.value), 700)}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <FilterBox setApiUrl={setApiUrl} setOptionUrl={setOptionUrl} />
+        <FilterBox setOptionUrl={setOptionUrl} />
       </Flex>
 
       <Flex
@@ -80,13 +68,6 @@ const ListOfCountries = ({ cca3 }) => {
           })
         )}
       </Flex>
-      <Center>
-        {dataLength < 12 || dataLength === limit ? null : (
-          <Button onClick={() => setLimit(limit + 12)} my="10">
-            Load More
-          </Button>
-        )}
-      </Center>
     </>
   );
 };
